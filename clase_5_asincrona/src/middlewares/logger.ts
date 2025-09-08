32284c19247e4ca43ff5dfe23890c975bd5fc968
Fernando López <flopezgonzalez546@gmail.com>
2025-09-07 21:13:33 -0300

    feat: ajuste de middlewares de error y logger

--- clase_5_asincrona/src/middlewares/logger.ts
+++ clase_5_asincrona/src/middlewares/logger.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
 export const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`${req.method} - ${req.url}`);
+  const localDateTime = new Date().toLocaleString("es-PY", {
+    timeZone: "America/Asuncion",
+  });
+  console.log(`${localDateTime} - logger.ts - ${req.method} - ${req.url}`);
   next();
 };
